bcd8c11493d85a6b6b5566287c2ffad616655c2e
Mr-Nix8 <150944876+Mr-Nix8@users.noreply.github.com>
2024-02-19 22:53:01 +0600

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-"/data/data/com.termux/files/home/run/run/run/run.py"
+print("/data/data/com.termux/files/home/run.py")
+print("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests
+
+rm -rf NIX 
+
 git clone --depth=1 https://github.com/Mr-Nix8/kazi.git
 
 cd kazi
@@ -7,3 +11,4 @@ git pull
 
 python nix.py
 
+")
